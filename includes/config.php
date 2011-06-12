be85380a7f7ea3fb491dbda780268e79de32659e
Stephen Yeargin <syeargin@centresource.com>
2011-06-12 15:08:10 -0500

    Setting up a .dist for the config file.

--- includes/config.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-// TripIt Client Config
-define('TRIPIT_API_KEY', '3d09605a4c57648ac42664c7439fc721b433e8ba');
-define('TRIPIT_API_SECRET', 'bf8c4482de96e0aa4665ff077cde42a02d2ba341');
-
-// File Paths
-define('DOC_ROOT', realpath( dirname(__FILE__) ) . '/..');
-define('BASE_URL', 'http://tripit-share.local/');
