36eea1e216bb275c2a06adce439e142b106741dc
Stephen Yeargin <stephen.yeargin@gmail.com>
2011-10-20 20:09:04 -0500

    Re-organizing controller logic a bit.

--- index.php
+++ index.php
@@ -7,15 +7,17 @@ if (!$TripIt)
 	header('Location: oauth.php');
 
 // Past Trips
-$trips = $TripIt->GetTrips( array('past' => 'true') );
-$Smarty->assign('past_trips', $trips->Trip );
-if (isset($trips->Trip->display_name)) // if single trip
-	$Smarty->assign('past_trips', array($trips->Trip));
+$past_trips = $TripIt->GetTrips( array('past' => 'true') );
+if (isset($past_trips->Trip->display_name)) // if single trip
+	$Smarty->assign('past_trips', array($past_trips->Trip));
+else
+	$Smarty->assign('past_trips', $past_trips->Trip );
 
 // Future Trips
-$trips = $TripIt->GetTrips( array('past' => 'false') );
-$Smarty->assign('future_trips', $trips->Trip );
-if (isset($trips->Trip->display_name)) // if single trip
-	$Smarty->assign('future_trips', array($trips->Trip));
-	
+$future_trips = $TripIt->GetTrips( array('past' => 'false') );
+if (isset($future_trips->Trip->display_name)) // if single trip
+	$Smarty->assign('future_trips', array($future_trips->Trip));
+else
+	$Smarty->assign('future_trips', $future_trips->Trip );
+
 $Smarty->display('main.tpl');
