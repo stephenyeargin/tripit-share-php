113b096be730b4ed9d7088ea9204ca7654129ab3
Stephen Yeargin <stephen.yeargin@gmail.com>
2011-10-21 20:48:09 -0500

    Spaces to tabs. If only for consistency.

--- includes/class.geocalculator.php
+++ includes/class.geocalculator.php
@@ -82,14 +82,14 @@ class GeoCalculator {
 		$delta_lat = $lat2 - $lat1 ;
 		$delta_lon = $lon2 - $lon1 ;
 
-	  $alpha    = $delta_lat/2;
-	  $beta     = $delta_lon/2;
-	  $a        = sin(deg2rad($alpha)) * sin(deg2rad($alpha)) + cos(deg2rad($lat1)) * cos(deg2rad($lat2)) * sin(deg2rad($beta)) * sin(deg2rad($beta)) ;
-	  $c        = asin(min(1, sqrt($a)));
-	  $distance = 2*self::$earth_radius * $c;
-	  $distance = round($distance, 4);
+		$alpha		= $delta_lat/2;
+		$beta			= $delta_lon/2;
+		$a				= sin(deg2rad($alpha)) * sin(deg2rad($alpha)) + cos(deg2rad($lat1)) * cos(deg2rad($lat2)) * sin(deg2rad($beta)) * sin(deg2rad($beta)) ;
+		$c				= asin(min(1, sqrt($a)));
+		$distance = 2*self::$earth_radius * $c;
+		$distance = round($distance, 4);
 
-	  return (float) $distance;
+		return (float) $distance;
 	}
 
 }
